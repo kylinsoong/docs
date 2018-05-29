dc2b8666d39813491a351da83d315eb165259742
kylin <kylinsoong.1214@gmail.com>
2018-05-29 14:15:41 +0800

    add new change

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "--------------------------------------"
+echo "Deploy Document"
+echo "--------------------------------------"
+echo ""
+
+DOCUMENT_VERSION_CURRENT=2018
+DOCUMENT_VERSION=$1
+
+echo "Check out to branch gh-pages"
+git checkout gh-pages
+
+if [ -z != $DOCUMENT_VERSION ]
+then
+echo "Document version is $DOCUMENT_VERSION"
+else
+DOCUMENT_VERSION=$DOCUMENT_VERSION_CURRENT
+echo "Document version is null, use default $DOCUMENT_VERSION"
+fi
+
+gitbook build ./ $DOCUMENT_VERSION
+
+echo "Push to Github"
+
+git status
+echo "Add all"
+git add --all
+echo "Commit"
+git commit -m "Deploy document"
+echo "pushing"
+git push origin gh-pages
+echo "Check out to master"
+git checkout master
