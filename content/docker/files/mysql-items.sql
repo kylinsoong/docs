8bcddd2d963143c0478b0058d9e0145a08a0ebb3
kylin <kylinsoong.1214@gmail.com>
2018-06-01 09:37:06 +0800

    Deploy document

---
+++ content/docker/files/mysql-items.sql
@@ -0,0 +1,3 @@
+CREATE TABLE Projects (id int(11) NOT NULL, name varchar(255) DEFAULT NULL, code varchar(255) DEFAULT NULL, PRIMARY KEY (id));
+INSERT INTO Projects (id, name, code) VALUES (1,'DevOps','DO180');
+INSERT INTO Projects (id, name, code) VALUES (2,'DevOps','DO280');
