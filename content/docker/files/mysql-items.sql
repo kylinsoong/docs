610ef869deb3ebcdad2b2cf16b91fd546529bb9f
Kylin Soong <kylinsoong.1214@gmail.com>
2019-06-20 16:52:15 +0800

    New Release: 2.1

--- content/docker/files/mysql-items.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE Projects (id int(11) NOT NULL, name varchar(255) DEFAULT NULL, code varchar(255) DEFAULT NULL, PRIMARY KEY (id));
-INSERT INTO Projects (id, name, code) VALUES (1,'DevOps','DO180');
-INSERT INTO Projects (id, name, code) VALUES (2,'DevOps','DO280');
